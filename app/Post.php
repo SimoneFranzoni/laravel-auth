f6955d4418380259d871e2ced574d42771e47a63
SimoneFranzoni <simofranzoni99@gmail.com>
2022-02-02 17:38:40 +0100

    Creazione Faker

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    
+    public static function generateSlug($title){
+
+        $slug = $title;
+        return $slug;
+    }
+
 }
