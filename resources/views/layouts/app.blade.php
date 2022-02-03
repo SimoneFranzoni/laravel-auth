e04a61045c32577bada64c46763d4086e23e6e94
SimoneFranzoni <simofranzoni99@gmail.com>
2022-02-03 15:01:04 +0100

    Errore nel faker/seeder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,12 @@
                 <a class="navbar-brand" href="{{ route('home') }}">
                     Vai al sito pubblico
                 </a>
+                {{-- In auth si vedono le cose solo se hai loggato --}}
+                @auth
+                    <a href="{{ route('admin.posts.index')}}">
+                        Vai a elenco post
+                    </a>                    
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
